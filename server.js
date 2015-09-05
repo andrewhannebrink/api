fa241529630d2629fda684daf1275e2bae92be54
Ubuntu <ubuntu@ip-172-31-43-151.us-west-2.compute.internal>
2015-09-05 20:45:23 +0000

    added favicon and appropriate port prerouting

--- server.js
+++ server.js
@@ -3,12 +3,14 @@
   // set up ========================
   var express  = require('express');
   var app = express();                               // create our app w/ express
+  var path = require('path');
   var multer = require('multer');
   var exec = require('child_process').exec;
+  var favicon = require('serve-favicon');
   var remojiProc;
   var rmProc;
   var convertProc;
-  var mongoose = require('mongoose');                     // mongoose for mongodb
+  //var mongoose = require('mongoose');                     // mongoose for mongodb
   var morgan = require('morgan');             // log requests to the console (express4)
   var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
   var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
@@ -16,14 +18,16 @@
 
   // configuration =================
 
-  mongoose.connect('mongodb://node:nodeuser@localhost:27017/icondb');     // connect to mongoDB database on modulus.io
+  //mongoose.connect('mongodb://node:nodeuser@localhost:27017/icondb');     // connect to mongoDB database on modulus.io
 
-  app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+  app.use(express.static('public'));                 // set the static files location /public/img will be /img for users
   app.use(morgan('dev'));                                         // log every request to the console
   app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
   app.use(bodyParser.json());                                     // parse application/json
   app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
   app.use(methodOverride());
+  app.use(favicon(path.join(__dirname + '/public/img/favicon.ico')));
+
 
   var fileTooLarge = false; //TODO block large files from working
 
